e88b80a80d3c356ff25cc4bb21cd4f625316d5dd
Ryo <kuttc777@gmail.com>
2016-10-31 23:39:54 +0900

    [edit] get number of rows in database table
    discribing number on index.jsp

--- src/main/java/com/mycode/AccessToDatabase.java
+++ src/main/java/com/mycode/AccessToDatabase.java
@@ -19,6 +19,7 @@ public class AccessToDatabase {
      String url =  "jdbc:postgresql://localhost/postgres";
      String user = "postgres";
      String password = "postgres";
+     int numberOfRow = 0;
      
 
     public AccessToDatabase() throws SQLException, ClassNotFoundException {
@@ -34,10 +35,32 @@ public class AccessToDatabase {
             //int code = executeQuery.getInt("code");
             String company = executeQuery.getString("color");
             System.out.println("コード 会社名:" + company);
+            numberOfRow ++;
+            System.out.println(new Integer(numberOfRow).toString());
         }
+        //executeQuery.last();
         stmt.close();
         executeQuery.close();
         
         
     }
+    
+    public int getDataFromDatabase() throws SQLException, ClassNotFoundException{
+        System.out.println(new Integer(numberOfRow).toString());
+        return numberOfRow;
+        }
+    //この書き方だめっぽい。
+//        public ResultSet getDataFromDatabase() throws SQLException, ClassNotFoundException{
+//        Class.forName("org.postgresql.Driver"); // PostgreSQLの場合 
+//        Connection con = DriverManager.getConnection(url, user, password);
+//        System.out.print("conection ok");
+//        ResultSet executeQuery;
+//         try (java.sql.Statement stmt = con.createStatement() //なぜこの書き方？
+//         ) {
+//             String sql = "select * FROM playground;";
+//             // テーブル照会実行
+//             executeQuery = stmt.executeQuery(sql);
+//         }
+//        return executeQuery;
+//    }
 }
--- src/main/java/com/mycode/Article.java
+++ src/main/java/com/mycode/Article.java
@@ -1,5 +1,8 @@
 package com.mycode;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Article {
     public int totalArticlesNumber = 1234;
     ConnectDatas datas;
@@ -11,8 +14,16 @@ public class Article {
       datas = new ConnectDatas();
     }
     
-    public int intest(){
-        return 0;
+    public int intest() throws SQLException, ClassNotFoundException{
+        //return 0;
+        AccessToDatabase atc = new AccessToDatabase();
+//        ResultSet dataFromDatabase = atc.getDataFromDatabase();
+//        System.out.println("1111111111");
+//        //件数を取得する処理
+//        dataFromDatabase.last();
+//        int numberOfRow = dataFromDatabase.getRow();
+//        System.out.println("4444444444");
+        return atc.getDataFromDatabase();
     }
     
     public int getTotalArticlesNumber(){
